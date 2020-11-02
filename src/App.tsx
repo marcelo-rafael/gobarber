09b36ea9a26471cbd5f31e51f6bc3ecebbb3dd97
marcelo-rafael <marcelo.rafael.goncalves@gmail.com>
2020-11-02 17:24:49 -0300

    API de Contexto

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Marcelo' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );
