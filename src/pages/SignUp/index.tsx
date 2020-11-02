ee7055f7a5a15005446d5ebb099fe8ca3967bfc8
marcelo-rafael <marcelo.rafael.goncalves@gmail.com>
2020-11-02 14:50:03 -0300

    validação de login

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -24,7 +24,7 @@ const SignUp: React.FC = () => {
         email: Yup.string()
           .required('E-mail obrigatório')
           .email('Digite um e-mail válido'),
-        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+        password: Yup.string().required('Senha obrigatória'),
       });
 
       await schema.validate(data, {
